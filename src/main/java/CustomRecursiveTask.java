4421edd889f07e621f9ed42c9360568b2ce96328
Superman696 <61060058+Superman696@users.noreply.github.com>
2020-07-06 12:51:23 +0300

    renamed method

--- src/main/java/CustomRecursiveTask.java
+++ src/main/java/CustomRecursiveTask.java
@@ -6,35 +6,35 @@ import java.util.concurrent.RecursiveTask;
 
 public class CustomRecursiveTask extends RecursiveTask<Long> {
     private static final int THRESHOLD = 250_000;
-    private final List<Integer> randomListOfNumbers;
+    private final List<Integer> numbers;
 
-    public CustomRecursiveTask(List<Integer> randomListOfNumbers) {
-        this.randomListOfNumbers = randomListOfNumbers;
+    public CustomRecursiveTask(List<Integer> numbers) {
+        this.numbers = numbers;
     }
 
     @Override
     protected Long compute() {
-        if (randomListOfNumbers.size() > THRESHOLD) {
+        if (numbers.size() > THRESHOLD) {
             return ForkJoinTask.invokeAll(createSubTasks())
                     .stream()
                     .mapToLong(ForkJoinTask::join)
                     .sum();
         } else {
-            return sum(randomListOfNumbers);
+            return sum(numbers);
         }
     }
  
     private Collection<CustomRecursiveTask> createSubTasks() {
         List<CustomRecursiveTask> dividedTasks = new ArrayList<>();
-        dividedTasks.add(new CustomRecursiveTask(randomListOfNumbers
-                        .subList(0, randomListOfNumbers.size() / 2)));
-        dividedTasks.add(new CustomRecursiveTask(randomListOfNumbers
-                .subList(randomListOfNumbers.size() / 2, randomListOfNumbers.size())));
+        dividedTasks.add(new CustomRecursiveTask(numbers
+                        .subList(0, numbers.size() / 2)));
+        dividedTasks.add(new CustomRecursiveTask(numbers
+                .subList(numbers.size() / 2, numbers.size())));
         return dividedTasks;
     }
  
-    private Long sum(List<Integer> randomListOfNumbers) {
-        return randomListOfNumbers.stream()
+    private Long sum(List<Integer> numbers) {
+        return numbers.stream()
                 .mapToLong(Integer::longValue)
                 .sum();
     }
